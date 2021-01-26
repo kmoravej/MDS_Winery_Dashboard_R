f1aeb53d5e7237018668b6aa5629a7d3001bcb0e
Neel Phaterpekar <nphaterp@ualberta.ca>
2021-01-26 07:29:02 -0800

    Added values to dropdown and ranges

--- app.R
+++ app.R
@@ -2,6 +2,10 @@ library(dash)
 library(dashCoreComponents)
 library(dashHtmlComponents)
 library(dashBootstrapComponents)
+library(tidyverse)
+
+df = read_csv('data/processed/cleaned_data.csv') %>% filter(price <= 100, points >= 80) 
+
 
 app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
 
@@ -14,42 +18,50 @@ app$layout(
                         htmlLabel('State Selection'),
                         dccDropdown(
                            # id = 'state-widget',
-                            options = list(list(label = "Country", value = "D"),
-                                           list(label = "San Francisco", value = "SF")),
+                            options = map(
+                                    unique(df$state), function(x){
+                                    list(label=x, value=x)
+                                        }),
                             value = 'select your state',
                             placeholder = 'Select a State'
                         ),
                         htmlLabel('Wine Type'),
                         dccDropdown(
                             #id = 'wine_variety',
-                            options = list(list(label = "Wine", value = "_"),
-                                           list(label = "Red", value = "_")),
+                            options = map(
+                                    unique(df$variety), function(x){
+                                    list(label=x, value=x)
+                                        }),
                             value = 'select a variety',
                             placeholder = 'Select a Variety'
                         ),
                         htmlLabel('Price Range'),
-                        dccSlider(
+                        dccRangeSlider(
                                 id = 'price',
-                                min = 1,
-                                max = 10,
+                                min = min(df$price),
+                                max = max(df$price),
                                 marks = list(
-                                    "1" = "1°C",
-                                    "5" = "5°C",
-                                    "10" = "10°C"
+                                    "4" = "4$",
+                                    "25" = "25$",
+                                    "50" = "50$",
+                                    "75" = "75$",
+                                    "100" = "100$"
                                 ),
-                                value = 5
+                                value = list(min(df$price), max(df$price))
                             ),
                        htmlLabel('Points Range'),
-                       dccSlider(
+                       dccRangeSlider(
                                 #id = 'points',
-                                min = 1,
-                                max = 10,
+                                min = min(df$points),
+                                max = max(df$points),
                                 marks = list(
-                                    "1" = "1°C",
-                                    "5" = "5°C",
-                                    "10" = "10°C"
+                                    "80" = "80",
+                                    "85" = "85",
+                                    "90" = "90",
+                                    "95" = "95",
+                                    "100" = "100"
                                 ),
-                                value = 5
+                                value = list(min(df$points), max(df$points))
                                 ),
                         htmlLabel('Value Ratio'),
                         dccSlider(
